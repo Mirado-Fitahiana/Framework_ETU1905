57f3c2bad8c8bef938e22f10a3b18184e1d15704
Mirado-Fitahiana <118985778+Mirado-Fitahiana@users.noreply.github.com>
2023-07-07 16:26:59 +0300

    sprint-5

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Creation classe mapping
     String method;
 Front Servlet :
     HashMap<String, Mapping> mappingUrls;  
-    
+
 -Sprint 3:
     Remplissage du  HashMap<String, Mapping> mappingUrls dans la fonction init
     Liste fonction annote
@@ -16,3 +16,6 @@ Front Servlet :
     
 -Sprint 4:
     Séparer dans framework les classes telles que : Frontservlet, Utils, Url et Mapping puis dans test-framework les classes de test, web.xml, fichiers jsp
+
+-Sprint 5:  
+    -Comparer l'url obtenu avec la liste annotations dans Hashmap.getKey, si il ya correspondance voir si la méthode retourne modelView et puis renvoyer dans sa page qui correspond à l'attribut vue dans ModelView
--- framework/src/FrontServlet.java
+++ framework/src/FrontServlet.java
@@ -2,9 +2,12 @@ package etu1905.framework.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Path;
+
 import jakarta.servlet.*;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
+// import jakarta.swing.text.Utilities;
+import jakarta.servlet.annotation.WebServlet;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +15,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import etu1905.framework.Mapping;
+import etu1905.framework.ModelView;
 import etu1905.framework.Url;
 import etu1905.framework.Utils;
 
@@ -24,13 +28,19 @@ public class FrontServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
+            //entrySet -> ampiasaina ao am boucle angalana an le clef sy valeur     
             out.println("You are being redirected to FRONTSERVLET");
-            for (Map.Entry<String, Mapping> entry : mappingUrls.entrySet()) {
+            ModelView modelView = (ModelView)Utils.modelDeRedirection(request, mappingUrls);
+            out.println(modelView.getVueRedirection() + " modelView.getVueRedirection()");
+            RequestDispatcher dispat = request.getRequestDispatcher(modelView.getVueRedirection());
+            dispat.forward(request, response);
+            /* for (Map.Entry<String, Mapping> entry : mappingUrls.entrySet()) {
                 String clef = entry.getKey();// clef
                 Mapping map = entry.getValue(); // valeur
                 out.println("L' annotation: " + clef + " de valeur " + map.getClassName() + " de fonction appelée "
                         + map.getMethod());
-            }
+            } */
+        //mappingUrls.entrySet();    
         } catch (Exception e) {
             out.println(e.getMessage() + "\n");
             e.printStackTrace();
